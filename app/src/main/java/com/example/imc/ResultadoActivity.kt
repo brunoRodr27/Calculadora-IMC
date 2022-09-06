b756ac67ccd0cb4bcc32d61a14dcf62eb130f4b0
Bruno Rodrigues <brunodasilvarodrigues.28@gmail.com>
2022-09-05 23:26:30 -0300

    A

---
+++ app/src/main/java/com/example/imc/ResultadoActivity.kt
@@ -0,0 +1,24 @@
+package com.example.imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_resultado2.*
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
+
+class ResultadoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado2)
+
+        val imc = intent.getParcelableExtra<IMC>("value")
+        titleDensidadeCorporal.text = imc.nome.toString()
+        textViewHeaderDensidadeCorporal.text = imc.calcular()
+        textViewResultadoDensidadeCorporal1.text = String.format("%.2f", imc.imc)
+        textViewPeso.text = String.format("%.2f", imc.peso)
+        textViewAltura.text = String.format("%.2f", imc.altura)
+
+
+    }
+}
